cd973c9437c1b86a53b179532d58caf1bac975fd
Mo Talib <motalib8099@gmail.com>
2026-08-02 11:26:57 +0530

    Add Close by Esc

--- Modal window/script.js
+++ Modal window/script.js
@@ -6,15 +6,14 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document .querySelectorAll('.show-modal');
 
 const openModal = function(){
-        console.log('Button Clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
 }
-const closeModal = function(){
-    console.log('Button clicked');
 
+const closeModal = function(e){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
+
 } 
 
 for (let i = 0; i < btnsOpenModal.length; i++){
@@ -24,3 +23,9 @@ for (let i = 0; i < btnsOpenModal.length; i++){
 btnCloseModal.addEventListener('click',closeModal );
 overlay.addEventListener('click', closeModal);
 
+
+document.addEventListener('keydown',function(e){
+    if(e.key === "Escape" && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
