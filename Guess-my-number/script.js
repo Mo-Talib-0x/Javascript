d9532c122157e6647c4955b9ea8a7c315640eb3d
Mo Talib <motalib8099@gmail.com>
2026-08-02 21:21:45 +0530

    improve code

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,60 +1,54 @@
-'use strict'
+"use strict";
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-const scoreEl = document.querySelector('.score');
-const numberEl = document.querySelector('.number');
-const guessEl = document.querySelector('.guess');
-const messageEl = document.querySelector('.message');
+const scoreEl = document.querySelector(".score");
+const numberEl = document.querySelector(".number");
+const guessEl = document.querySelector(".guess");
+const messageEl = document.querySelector(".message");
+const highscoreEl = document.querySelector(".highscore");
 const body = document.body;
-const highscoreEl = document.querySelector('.highscore');
 
+const displayMessage = function (message) {
+  messageEl.textContent = message;
+};
 
-const displayMessage = function(message){
-    messageEl.textContent = message;
-}
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
 
-document.querySelector('.again').addEventListener('click',function(){
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    score = 20;
-
-    displayMessage("Start guessing...");
-    body.style.backgroundColor = '#222';
-    scoreEl.textContent = score;
-    numberEl.textContent = '?';
-    guessEl.value = '';
-
-
+  displayMessage("Start guessing...");
+  body.style.backgroundColor = "#222";
+  scoreEl.textContent = score;
+  numberEl.textContent = "?";
+  guessEl.value = "";
 });
 
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(guessEl.value);
 
-document.querySelector('.check').addEventListener('click',function(){
+  if (!guess) {
+    displayMessage("🚨 No number");
+  } else if (guess === secretNumber) {
+    displayMessage("✅ Correct Guess!");
+    body.style.backgroundColor = "#60b347";
+    numberEl.textContent = secretNumber;
 
-    const guess = Number(guessEl.value);
-
-    if(!guess){
-        displayMessage("🚨 No number");
-    }else if (guess === secretNumber){
-        displayMessage("✅ Correct Guess!");
-        body.style.backgroundColor = '#60b347';
-        numberEl.textContent = secretNumber;
-        if (score > highscore){
-            highscore = score;
-            highscoreEl.textContent = highscore;
-        }
-        
-
-    }else{
-        if(score > 1){
-            displayMessage((guess > secretNumber) ? '🚨 Too high' : '🚨 Too low');
-            score--;
-            scoreEl.textContent = score;
-        }else{
-            displayMessage("❌ Game Over");
-            score = 0;
-            scoreEl.textContent = score;
-        }
+    if (score > highscore) {
+      highscore = score;
+      highscoreEl.textContent = highscore;
     }
+  } else {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "🚨 Too high" : "🚨 Too low");
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      displayMessage("❌ Game Over");
+      score = 0;
+      scoreEl.textContent = score;
+    }
+  }
 });
