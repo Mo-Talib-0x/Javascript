cf2b0b8b763a597a207337b4b5f6d65d57816299
Mo Talib <motalib8099@gmail.com>
2026-08-16 23:04:03 +0530

    Challenge 2 complete

--- C_L_Functions/challenge2.js
+++ C_L_Functions/challenge2.js
@@ -4,10 +4,20 @@
   const header = document.querySelector("h1");
   header.style.color = "red";
   header.addEventListener("click", function () {
-    if (header.style.color === "red") {
-      header.style.color = "blue";
-    } else {
-      header.style.color = "red";
-    }
+    header.style.color = "blue";
   });
 })();
+
+
+// // Why it is working even after program execution is completed and stack is empty 
+// (function () {
+//   const header = document.querySelector("h1");
+//   header.style.color = "red";
+//   header.addEventListener("click", function () {
+//     if (header.style.color === "red") {
+//       header.style.color = "blue";
+//     } else {
+//       header.style.color = "red";
+//     }
+//   });
+// })();
