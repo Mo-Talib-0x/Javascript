92b37fe4dd2fec62593a8a18f1d8255437e60917
Mo Talib <motalib8099@gmail.com>
2026-08-21 14:44:08 +0530

    create DOM elements

--- WorkingWithArray/script.js
+++ WorkingWithArray/script.js
@@ -61,6 +61,30 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+
+  containerMovements.innerHTML = '';
+
+
+  movements.forEach(function (mov , i ){
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+            i +1
+          } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+
+        containerMovements.insertAdjacentHTML('beforeend', html);
+  });
+}
+
+displayMovements(account1.movements);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -72,24 +96,3 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-//for (const movement of movements){
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1} You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1} You withdraw ${Math.abs(movement)}`);
-  }
-}
-
-console.log('----FOREACH-----');
-
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i} You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i} You withdraw ${Math.abs(mov)}`);
-  }
-});
