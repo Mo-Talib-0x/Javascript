cd4eee36e1bcf8a3a3279dc19516f9f6a2e5e52a
Mo Talib <motalib8099@gmail.com>
2026-08-22 19:19:28 +0530

    map complete

--- WorkingWithArray/script.js
+++ WorkingWithArray/script.js
@@ -61,29 +61,38 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements){
-
+const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
 
-
-  movements.forEach(function (mov , i ){
+  movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
-            i +1
+            i + 1
           } ${type}</div>
           <div class="movements__value">${mov}</div>
         </div>
         `;
 
-        containerMovements.insertAdjacentHTML('beforeend', html);
+    containerMovements.insertAdjacentHTML('beforeend', html);
   });
-}
+};
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+      acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
