705a7e086c8ade2d4c7d64b396888714ef205856
Mo Talib <motalib8099@gmail.com>
2026-08-23 16:14:43 +0530

    complete challenge2

---
+++ WorkingWithArray/challenge2.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const checkDogs = function (data1, data2) {
+  const updatedData1 = data1.slice(1, -2);
+
+  const corrected = updatedData1.concat(data2);
+
+  corrected.forEach((element, i) => {
+    if (element >= 3) {
+      console.log(`Dog number ${i +1} is an adult, and is ${element} years old`);
+    }else{
+        console.log(`Dog number ${i +1} is still a puppy 🐶`);
+    }
+  });
+};
+
+const d1 = [3, 5, 2, 12, 7];
+const d2 = [4, 1, 15, 8, 3];
+
+checkDogs(d1, d2);
+
+const d3 = [9, 16, 6, 8, 3];
+const d4 = [10, 5, 6, 1, 4];
+
+console.log("----next-----");
+checkDogs(d3, d4)
+
+const  calcAverageHumanAge = function(ages){
+    const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+    const adults = humanAges.filter((age) => age >= 18);
+
+    console.log(humanAges);
+    console.log(adults);
+
+    const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+    return average;
+    
+}
+
+console.log(calcAverageHumanAge(d4));
+
