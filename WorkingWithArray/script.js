a5ca8bea39ae0b64ad4b3beec8816a32de406d50
Mo Talib <motalib8099@gmail.com>
2026-09-03 12:48:15 +0530

    Practice Array Methods

--- WorkingWithArray/script.js
+++ WorkingWithArray/script.js
@@ -199,7 +199,7 @@ btnLoan.addEventListener('click', function (e) {
 
 let sorted = false;
 
-btnSort.addEventListener('click', function(e){
+btnSort.addEventListener('click', function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
@@ -231,8 +231,6 @@ const totalAmout1 = accounts
 
 console.log(totalAmount);
 
-
-
 labelBalance.addEventListener('click', function () {
   const movementsUI = Array.from(
     document.querySelectorAll('.movements__value'),
@@ -241,7 +239,59 @@ labelBalance.addEventListener('click', function () {
 
   const movementsUI2 = [...document.querySelectorAll('.movements__value')];
 
-  console.log(movementsUI2.map(el => Number(el.textContent.replace('€', ''))))
+  console.log(movementsUI2.map(el => Number(el.textContent.replace('€', ''))));
 
   console.log(movementsUI);
 });
+
+// Array Methods Practice
+// 1.
+const bankDeposit = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDeposit);
+
+// // 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 1000)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(numDeposits1000);
+
+// 2.
+const numDeposits1000_2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? count + cur : count), 0);
+console.log(numDeposits1000_2);
+
+// 3.
+const {deposits, withdrawals} = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 },
+  );
+// console.log(sums);
+
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice title --> This Is a Nice Title
+const convertTitleCase = function(title){
+
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title.toLowerCase().split(' ').map(word => exceptions.includes(word) ? word : capitalize(word)).join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is the another title with an Example'));
