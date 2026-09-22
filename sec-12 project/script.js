e7f71c580345c084648fc6e76fafdfd6c2934978
Mo Talib <motalib8099@gmail.com>
2026-09-22 23:11:33 +0530

    complete math and rounding

--- sec-12 project/script.js
+++ sec-12 project/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -111,12 +111,12 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -288,3 +288,46 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+
+console.log("Math and rounding");
+
+console.log(Math.sqrt(25));
+console.log(25 ** 1/2);
+console.log(8 ** 1/3);
+
+console.log(Math.max(2, 34, 65, 23));
+console.log(Math.max(2, 34, '65', 23));
+console.log(Math.max(2, 34, '65px', 23));
+
+console.log(Math.min(2, 34, 65, 23));
+
+console.log(Math.PI * Number.parseFloat('10px' ** 2));
+
+console.log(Math.trunc(Math.random() * 6) +1);
+
+const randomInt = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(4, 65));
+console.log(randomInt(0, 3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(23.3));
+console.log(Math.trunc(-23.3));
+
+console.log(Math.floor(-23.3));
+
+console.log((2.7).toFixed(0));
+
+// Rounding decimals
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
