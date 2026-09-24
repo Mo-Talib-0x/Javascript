5433a47e3072ed8b86733a201ac314c2c2a40215
Mo Talib <motalib8099@gmail.com>
2026-09-24 12:50:27 +0530

    complete remainder opeartor

--- sec-12 project/script.js
+++ sec-12 project/script.js
@@ -331,3 +331,22 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// Remainder
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(6));
+console.log(isEven(7));
+
+labelBalance.addEventListener('click', function(){
+
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i){
+    if(i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if(i % 3 === 0) row.style.backgroundColor = 'blue'
+  });
+})
+
+
